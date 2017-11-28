4a6aca3ccedba83b03ad2ec04e936754c219c36b
sanchikgn <kasumisanchika@gmail.com>
2017-11-28 14:34:58 +0530

    Notify Cursor Updates

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var io = require('socket.io')(http);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var Room = require('./room.js');
 
+//Room concept-------------
+var Room = require('./room.js');
+var allRooms = {}; //all the available rooms
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,34 +31,55 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 //socketio -------------------------------------------------------------------------------------------------
 io.on('connection', function (socket) {
     console.log('new connection ...');
 
-    //Connect to room-------------------------------------------------------
+    //Connect to room -------------------------------------------------------
     socket.on('joinroom', function(user) {
-        var username = user;
+        var username = user.username;
+        var joinedRoom  = user.room;
         console.log(user);
-        console.log(user.room);
-        console.log(user.username);
+        var room = allRooms[joinedRoom]; //retrieve the relevant room object
+        if(room == null) { //if room has not been created
+            if(allRooms.length == null)
+                id = 0;
+            else id = allRooms.length++;
+            room = new Room(joinedRoom); //create a room object
+            allRooms[joinedRoom] = room;
+        }
+        console.log(room);
+        room.addPerson(username); //add the person to the room
+        socket.join(joinedRoom); //join the room
+        //notify others in room------------------------------------
+        io.sockets.in(joinedRoom).emit('userjoin', {msg:"You have joined " + joinedRoom,
+            users: room.getUsersInRoom()});
     });
 
+    //Onclick by user -----------------------------------------------------------
+    socket.on('onclick', function (update) {
+        //update = {X, Y, username, room}
+        var X = update.setX;
+        var Y = update.setY;
+        //notify others in room----------------------------------
+        io.sockets.in(update.room).emit('update-position', update);
+    })
 });
 
 http.listen(3030, function(){
